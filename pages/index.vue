f5e0fd2faa104a9a40f3b6811cadec7e815b29fd
Bjerkhaug Jarand <Jarand.Bjerkhaug@telenor.com>
2019-06-06 20:08:48 +0200

    meh

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container" :class="stuff.class">
     <div>
       <h1 class="title">{{stuff.text}}</h1>
-      <img :src="stuff.giphy" alt="">
+      <img :src="stuff.giphy" alt>
       <h2 v-html="stuff.description" class="description"></h2>
     </div>
   </div>
@@ -14,23 +14,20 @@ export default {
 
   computed: {
     stuff: function() {
+      moment.locale('nb')
       const currentDate = moment();
       const currentMonth = currentDate.month();
       const currentYear = currentDate.year();
       const nextYear = currentDate.year(currentYear + 1).year();
 
-      console.log(currentMonth);
-      console.log(currentYear);
-      console.log(nextYear);
-      // const nextJune = moment();
-      // console.log("next", nextJune);
+      const daysToPride = moment([nextYear, 5, 1]).toNow(true);
 
-      if(currentMonth === 5)
+      if(currentMonth === 3)
       {
-        return {text: "YAAAS", description: "Happy <span> pride </span>", giphy: "https://media.giphy.com/media/3o7TKKFRj4mQDFZYhG/giphy.gif", class: "isPride", countdown: false}
+        return {text: "YAAAS", description: 'Happy <span class="p">p</span><span class="r">r</span><span class="i">i</span><span class="d">d</span><span class="e">e</span>', giphy: "https://media.giphy.com/media/3o7TKKFRj4mQDFZYhG/giphy.gif", class: "isPride", countdown: false}
       }
       else {
-        return {text: "Nei", description: `... Men det er bare <span> ${nextYear} </span> dager igjen!`, giphy: "https://media.giphy.com/media/DnlUipYM3lMGc/giphy.gif", class: "", countdown: true}
+        return {text: "Nei...", description: `... Men det er bare <span> ${daysToPride} </span> igjen!`, giphy: "https://media.giphy.com/media/DnlUipYM3lMGc/giphy.gif", class: "notPride", countdown: true}
       }
 
       return today;
@@ -39,7 +36,7 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -51,18 +48,55 @@ export default {
   background-position: center;
   background-repeat: no-repeat;
   background-image: url("~assets/bakgrunn_ikkepride.jpg");
+}
 
-  &.isPride {
-    background-image: url("~assets/Pridebakgrunn.jpg");
+h1 {
+  font-size: 100px;
+  color: maroon;
+}
 
-    .description, .title {
-      font-size: 100px;
+h2 {
+  font-size: 50px;
+  max-width: 420px;
+  text-align: center;
+}
 
-      span {
-        color: rebeccapurple;
-        text-transform: uppercase;
-      }
+.isPride {
+  background-image: url("~assets/Pridebakgrunn.jpg");
+
+  h1,
+  h2 {
+    color: black;
+    font-size: 100px;
+    max-width: none;
+    text-transform: uppercase;
+
+  }
+  h2 {
+    font-family: cursive;
+  }
+}
+
+.p {
+    color: red;
     }
+  .r {
+    color: yellow;
+  }
+  .i {
+    color: green;
+  }
+  .d {
+    color: blue;
+  }
+  .e {
+    color: blueviolet;
+  }
+
+.notPride {
+  h2 span {
+    display: block;
+    color: maroon;
   }
 }
 </style>
