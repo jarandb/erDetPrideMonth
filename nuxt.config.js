b9504e3beafd5efd47194b5e80dbdd82ad7f5583
Bjerkhaug Jarand <Jarand.Bjerkhaug@telenor.com>
2019-06-07 11:23:10 +0200

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,53 +1,56 @@
 export default {
-  mode: 'spa',
+  mode: "spa",
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-
-    title: 'Er det pride month?',
+    title: "Er det pride month?",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
+    ],
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "favicon.png"
+      }
     ]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
 
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
 
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
-
-
-
+   ** Nuxt.js modules
+   */
+  modules: [],
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
